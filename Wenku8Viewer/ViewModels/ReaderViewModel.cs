4d7914f24efec8e4bb193e6afcd084dbbf42fe60
djdjz7 <djdjz7@icloud.com>
2024-06-22 23:16:24 +0800

    feat: prev&next

--- Wenku8Viewer/ViewModels/ReaderViewModel.cs
+++ Wenku8Viewer/ViewModels/ReaderViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Reactive;
@@ -21,14 +22,30 @@ public class ReaderViewModel : ViewModelBase, IRoutableViewModel
     {
         HostScreen = screen;
         _browsingContext = browsingContext;
-        _chapterUrl = chapterUrl;
+        _chapterUrl = new Uri(chapterUrl);
+        SwitchPreviousCommand = ReactiveCommand.CreateFromTask(
+            async () =>
+            {
+                await SwitchChapter(PreviousUrl!);
+            },
+            this.WhenAnyValue(x => x.PreviousUrl, (x) => !string.IsNullOrEmpty(x))
+        );
+        SwitchNextCommand = ReactiveCommand.CreateFromTask(
+            async () =>
+            {
+                await SwitchChapter(NextUrl!);
+            },
+            this.WhenAnyValue(x => x.NextUrl, (x) => !string.IsNullOrEmpty(x))
+        );
     }
 
     public IScreen HostScreen { get; }
     private IBrowsingContext _browsingContext;
-    private string _chapterUrl = null!;
+    private Uri _chapterUrl = null!;
     private string? _chapterContent;
     private string? _chapterTitle;
+    private string? _previousUrl;
+    private string? _nextUrl;
     public string? ChapterContent
     {
         get => _chapterContent;
@@ -39,14 +56,50 @@ public class ReaderViewModel : ViewModelBase, IRoutableViewModel
         get => _chapterTitle;
         set => this.RaiseAndSetIfChanged(ref _chapterTitle, value);
     }
-    public string? UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
-    public async void OnLoaded()
+    public string? PreviousUrl
     {
-        var document = await _browsingContext.OpenAsync(_chapterUrl);
+        get => _previousUrl;
+        set => this.RaiseAndSetIfChanged(ref _previousUrl, value);
+    }
+    public string? NextUrl
+    {
+        get => _nextUrl;
+        set => this.RaiseAndSetIfChanged(ref _nextUrl, value);
+    }
+    public string? UrlPathSegment { get; } = Guid.NewGuid().ToString().Substring(0, 5);
+    public ReactiveCommand<Unit, Unit> SwitchPreviousCommand { get; }
+    public ReactiveCommand<Unit, Unit> SwitchNextCommand { get; }
+
+    public async Task OnLoaded()
+    {
+        var document = await _browsingContext.OpenAsync(_chapterUrl.ToString());
         var contentDiv = document.QuerySelector("#content");
         var textContent = contentDiv?.TextContent ?? string.Empty;
-        ChapterContent = NovelUtils.GetNovelContentReplaceRegex().Replace(textContent, string.Empty);
+        ChapterContent = NovelUtils
+            .GetNovelContentReplaceRegex()
+            .Replace(textContent, string.Empty);
         var titleDiv = document.QuerySelector("#title");
         ChapterTitle = titleDiv?.TextContent ?? string.Empty;
+        var footTextDiv = document.QuerySelector("#foottext");
+        if (footTextDiv is null)
+            return;
+        var prevA = footTextDiv.QuerySelector("a:nth-child(3)");
+        var nextA = footTextDiv.QuerySelector("a:nth-child(4)");
+        var prevUrl = prevA?.Attributes["href"]?.Value ?? string.Empty;
+        var nextUrl = nextA?.Attributes["href"]?.Value ?? string.Empty;
+        if(prevUrl.Contains("index"))
+            PreviousUrl = string.Empty;
+        else
+            PreviousUrl = prevUrl;
+        if(nextUrl.Contains("last"))
+            NextUrl = string.Empty;
+        else
+            NextUrl = nextUrl;
+    }
+
+    public async Task SwitchChapter(string url)
+    {
+        _chapterUrl = new Uri(_chapterUrl, url);
+        await OnLoaded();
     }
 }
